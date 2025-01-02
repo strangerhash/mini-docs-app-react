a9990bd2d03665f47510c14460ca326f5a4414bb
Satyendra Pandey <pandeysatyendra870@gmail.com>
2025-01-02 10:35:57 +0530

    Mini Docs APP

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Background from './components/background'
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold'>Documents.</div>
+      <h1 className='absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 text-[13vw] leading-none tracking-tighter font-semibold text-zinc-200'>Docs.</h1>
+    </>
+  )
+}
+
+export default Background
